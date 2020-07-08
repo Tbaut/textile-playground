6747bc050bdf231f7d1fe63cf9a8cf9a4b971dc5
thib <thibaut.sardan@gmail.com>
2020-07-09 01:01:49 +0200

    creating DB, writing data and retreiving

--- src/components/Threadtest.tsx
+++ src/components/Threadtest.tsx
@@ -1,52 +1,88 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { createAPISig, Client, KeyInfo, ThreadID, UserAuth } from '@textile/hub';
-import { DBInfo } from '@textile/threads-client';
+import { Libp2pCryptoIdentity } from '@textile/threads-core';
+import { Where } from '@textile/threads-client';
 
 const key = process.env.REACT_APP_HUB_KEY || '';
 const secret = process.env.REACT_APP_HUB_SECRET || '';
+enum collection {
+  POST = 'post',
+  COMMENT = 'comment',
+}
 
-const personSchema = {
-  $id: 'https://example.com/person.schema.json',
+// Q: is string for createdAd the best?
+interface Post {
+  _id: string;
+  title: string;
+  content: string;
+  author: string;
+  createdAd: string;
+}
+
+interface Comment {
+  _id: string;
+  postId: string;
+  title: string;
+  content: string;
+  author: string;
+  createdAd: string;
+}
+
+const postSchema = {
   $schema: 'http://json-schema.org/draft-07/schema#',
-  title: 'Person',
+  title: 'Post',
   type: 'object',
   required: ['_id'],
   properties: {
     _id: {
       type: 'string',
-      description: "The instance's id.",
+      description: 'The post id.',
     },
-    firstName: {
+    title: {
       type: 'string',
-      description: "The person's first name.",
+      description: 'This post title.',
     },
-    lastName: {
+    content: {
       type: 'string',
-      description: "The person's last name.",
+      description: 'This post content.',
     },
-    age: {
-      description: 'Age in years which must be equal to or greater than zero.',
-      type: 'integer',
-      minimum: 0,
+    author: {
+      type: 'string',
+      description: 'The author id.',
+    },
+    createdAt: {
+      type: 'string',
+      description: 'The creation date of the post',
     },
   },
 };
 
-// Minimal schema representation
-const schema2 = {
+const commentSchema = {
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  title: 'Comment',
   properties: {
-    _id: { type: 'string' },
-    fullName: { type: 'string' },
-    age: { type: 'integer', minimum: 0 },
+    _id: {
+      type: 'string',
+      description: "The instance's (comment) id.",
+    },
+    postId: {
+      type: 'string',
+      description: "The forum post's id to which the comment is linked to.",
+    },
+    content: {
+      type: 'string',
+      description: 'This post content.',
+    },
+    author: {
+      type: 'string',
+      description: 'The author id.',
+    },
+    createdAt: {
+      type: 'string',
+      description: 'The creation date of the post',
+    },
   },
 };
-
-interface Person {
-  _id: string;
-  firstName: string;
-  lastName: string;
-  age: number;
-}
 
 async function getClientWithKeyInfo(): Promise<Client> {
   const keyInfo: KeyInfo = {
@@ -58,17 +94,15 @@ async function getClientWithKeyInfo(): Promise<Client> {
   return client;
 }
 
-async function setup(auth?: UserAuth): Promise<void> {
+async function setup(): Promise<any> {
   if (!key || !secret) {
     throw 'REACT_APP_HUB_KEY and REACT_APP_HUB_SECRET need to be set as env variable';
   }
 
   const user = await Client.randomIdentity();
-
-  // for our user
-  // const secondsExpiration = 3600;
-  // const expiration = new Date(Date.now() + 1000 * secondsExpiration);
-  // const apiSig = await createAPISig(secret, expiration);
+  // console.log('usertoString', user.toString());
+  const test = Libp2pCryptoIdentity.fromString(user.toString());
+  // console.log('test', (await test).toString());
 
   const client = await getClientWithKeyInfo();
 
@@ -77,58 +111,89 @@ async function setup(auth?: UserAuth): Promise<void> {
   const token = await client.getToken(user);
 
   const threadId = ThreadID.fromRandom();
-  console.log('ThreadID', threadId.toString());
 
-  const thread = await client.newDB(threadId, 'space');
+  const thread = await client.newDB(threadId, 'open-polkassembly');
   const threadString = thread.toString();
 
-  const info = await client.getDBInfo(threadId);
-  console.log('info', info);
+  await client.newCollection(thread, collection.POST, postSchema);
+  await client.newCollection(thread, collection.COMMENT, commentSchema);
 
-  // This will be overriden right after
-  // const register = await client.newCollection(thread, 'Person', personSchema);
-  // console.log('register', register);
-
-  const astronaut = {
-    _id: '',
-    name: 'Buzz',
-    missions: 13,
-  };
-  const collectionName = 'astronauts';
-
-  // create collection named $collectionName from the previously defined astronaut object
-  await client.newCollectionFromObject(thread, collectionName, astronaut);
-  await client.create(thread, collectionName, [{ ...astronaut }]);
-  const data = await client.find(thread, collectionName, {});
-
+  await client.create(thread, collection.POST, [
+    {
+      _id: '',
+      author: 'john',
+      content: 'that is the first post',
+      createdAd: Date.now().toString(),
+      title: 'Number one',
+    } as Post,
+  ]);
+  const data = await client.find(thread, collection.POST, {});
   console.log('data', data);
-  // // create a new instance un the collection $collectionName
-  // await client.create(thread, collectionName, [{ name: 'Bla', missions: 11 }]);
 
-  // console.debug('found:', found.instancesList);
+  await client.create(thread, collection.COMMENT, [
+    {
+      _id: '',
+      author: 'jen',
+      content: 'And a comment to our first post',
+      createdAd: Date.now().toString(),
+      postId: (data.instancesList[0] as Post)._id,
+    } as Comment,
+  ]);
+
   const user2 = await Client.randomIdentity();
 
-  // for our user
-  // const secondsExpiration = 3600;
-  // const expiration = new Date(Date.now() + 1000 * secondsExpiration);
-  // const apiSig = await createAPISig(secret, expiration);
-  const client2 = await getClientWithKeyInfo();
+  // for our front-end user
+  const secondsExpiration = 3600;
+  const expiration = new Date(Date.now() + 1000 * secondsExpiration);
+  const apiSig = await createAPISig(secret, expiration);
+  const client2 = await Client.withUserAuth({ key, sig: apiSig.sig, msg: apiSig.msg, token });
 
   // a new token needs to be created on the hub (otherwise it throws)
   // ? ----> But this token is never used
   const token2 = await client2.getToken(user2);
 
-  // const info2 = await client2.getThread('space');
   const thread2 = ThreadID.fromString(threadString);
-  const data2 = await client.find(thread2, collectionName, {});
+  const result = {};
+  const posts = await client.find(thread2, collection.POST, {});
+  const post0 = posts.instancesList[0];
+  const data2 = await client.find(thread2, collection.COMMENT, new Where('postId').eq(post0._id));
+  const comment = data2.instancesList[0];
 
-  console.log('data', data2);
+  return {
+    post: {
+      title: post0.title,
+      content: post0.content,
+    },
+    comment: {
+      content: comment.content,
+      createdAt: comment.createdAt,
+    },
+  };
 }
 
 const Threadtest = (): JSX.Element => {
-  setup();
+  const [thread, setThread] = useState<any>({});
 
-  return <>bla</>;
+  useEffect(() => {
+    setup()
+      .then((t) => setThread(t))
+      .catch((e) => console.error(e));
+  }, []);
+
+  if (!thread?.post?.title) {
+    return <>Loading...</>;
+  }
+
+  return (
+    <>
+      <h1>{thread.post?.title}</h1>
+      <p>{thread.post?.content}</p>
+      <hr />
+      <p>
+        {thread.comment.content} - {thread.comment.createdAt}
+      </p>
+    </>
+  );
 };
 
 export default Threadtest;
